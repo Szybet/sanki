b7ff75647e79d961c7e208d1d5e3a6649e7940d7
Szybet <53944559+Szybet@users.noreply.github.com>
2024-01-26 18:28:50 +0100

    create dirs for attach

--- anki-attach/anki-drop-install.sh
+++ anki-attach/anki-drop-install.sh
@@ -3,6 +3,9 @@
 cargo install ripdrag
 cargo install s2png
 
+mkdir -p ~/.local/share/applications/
+mkdir -p ~/.local/bin/
+
 cp anki-drop.desktop ~/.local/share/applications/
 cp anki-drop.sh ~/.local/bin/
 
