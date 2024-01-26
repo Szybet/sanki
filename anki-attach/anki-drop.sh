3c4cc652ceb23361ae6fc70bc75560a833894c47
Szybet <53944559+Szybet@users.noreply.github.com>
2024-01-26 20:02:21 +0100

    debug info

--- anki-attach/anki-drop.sh
+++ anki-attach/anki-drop.sh
@@ -9,7 +9,11 @@ file="$1"
 filename=$(basename "$file")
 filename_clear=$(remove_non_ascii "$filename")
 
+touch /tmp/debug-anki-drop
+echo $file >> /tmp/debug-anki-drop 2>&1
+echo $filename_clear >> /tmp/debug-anki-drop 2>&1
+
 rm -rf /tmp/anki-drop/
 mkdir /tmp/anki-drop/
-s2png -o "/tmp/anki-drop/${filename_clear}.png" -b "$filename_clear" -s "$file"
-ripdrag -x -a -n -d "/tmp/anki-drop/${filename_clear}.png"
+s2png -o "/tmp/anki-drop/${filename_clear}.png" -b "$filename_clear" -s "$file" >> /tmp/debug-anki-drop 2>&1
+ripdrag -x -a -n -d "/tmp/anki-drop/${filename_clear}.png" >> /tmp/debug-anki-drop 2>&1
